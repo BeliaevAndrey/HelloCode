285159d98f94600167ab0126df30b0ef2dec2e1f
Andrei Beliaev <abbot190414@gmail.com>
2022-04-10 00:09:04 +0300

    Example 004 Division

---
+++ Example004_Division/Program.cs
@@ -0,0 +1,12 @@
+﻿double numberA = new Random().Next(1, 100);
+Console.WriteLine("Number A = {0}", numberA);
+double numberB = new Random().Next(1, 100);
+Console.WriteLine("Number B = {0}", numberB);
+if(numberA > numberB)
+{
+Console.WriteLine("B/A = {0}", numberA / numberB);
+}
+else
+{
+Console.WriteLine("B/A = {0}", numberB / numberA);
+}
