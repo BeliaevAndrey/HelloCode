cfdffcd4169a99f10c5dca3bec8b4a4ae61ac46f
Andrei Beliaev <abbot190414@gmail.com>
2022-04-09 20:07:25 +0300

    Example 002 HelloUser added

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,21 @@
+﻿// second programm. Say 'Hello' to definite user.
+string username;
+for(int i = 0; i < 3; i++)
+{
+    Console.Write("Введите имя пользователя: ");
+    username = Console.ReadLine();
+
+    if(username.ToLower() == "masha")
+    {
+        Console.WriteLine("WOW! This is Masha!");
+    }
+    else if(username == "")
+    {
+        Console.WriteLine("Nobody came. =(");
+    }
+    else
+    {
+        Console.Write("Hello,  ");
+        Console.WriteLine(username);
+    }
+}
