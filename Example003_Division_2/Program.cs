8c78b0f3b467a81555413a6dae44fa44c85d2185
Andrei Beliaev <abbot190414@gmail.com>
2022-04-10 16:55:29 +0300

    Still stucked on parsing double

--- Example003_Division_2/Program.cs
+++ Example003_Division_2/Program.cs
@@ -1,11 +1,5 @@
 ﻿double numberA, numberB;
 numberA = Convert.ToDouble(Console.ReadLine());
 numberB = Convert.ToDouble(Console.ReadLine());
-if(numberA > numberB)
-{
+Console.WriteLine("A / B = {0} / {1}", numberA, numberB);
 Console.WriteLine("A/B = {0}", numberA / numberB);
-}
-else
-{
-Console.WriteLine("B/A = {0}", numberB / numberA);
-}
